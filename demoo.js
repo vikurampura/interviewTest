a68fb5142336567058ec731a5f747cdd2f8fd288
vikurampura <87867645+vikurampura@users.noreply.github.com>
2021-07-24 09:48:46 +0530

    Add files via upload

---
+++ demoo.js
@@ -0,0 +1,16 @@
+var n= 5;
+
+for(var i=n; i>=1; i--){
+    for(var j=1; j<=i; ++j){
+        console.log("* ");
+    }
+    console.log("/n");
+}
+
+
+var n = 3;
+for(var i=n; i>=1; i--){
+    for(var j=1; j<=i; ++j){
+        console.log("* ");
+    }    console.log("/n");
+}
